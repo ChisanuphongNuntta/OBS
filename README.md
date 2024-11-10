345a9a9c9c3bfdc1ddbb8774f8081726c94b7bd0
ChisanuphongNuntta <tvrocker.www@gmail.com>
2024-11-11 02:02:35 +0700

    Heart Dev

---
+++ README.md
@@ -0,0 +1,31 @@
+# Install OBS Client
+
+create file .sh
+```
+#!/bin/bash
+
+ACCESS_KEY=Your-access-key
+PRIVATE_KEY=Your-private-key
+echo 
+
+wget https://obs-community-intl.obs.ap-southeast-1.myhuaweicloud.com/obsutil/current/obsutil_linux_amd64.tar.gz
+
+tar -xzvf obsutil_linux_amd64.tar.gz
+cd obsutil_linux_amd64_*/
+chmod +x obsutil
+
+sudo mv obsutil /usr/local/bin/obsutil
+
+obsutil config -i $ACCESS_KEY -k $PRIVATE_KEY -e obs.ap-southeast-2.myhuaweicloud.com
+
+cd ..
+rm -rf obsutil_linux_amd64_*/
+rm obsutil_linux_amd64.tar.gz
+```
+
+## Don't forget change *ACCESS_KEY* and *PRIVATE_KEY*
+Then success edit save and run *bash*
+
+### Your config file location `~/.obsutilconfig`
+If want to change `ACCESS_KEY`(AK) and `PRIVATE_KEY`(SK)
+
---
+++ obs-dump-and-push.py
@@ -0,0 +1,77 @@
+import os
+import subprocess
+from datetime import datetime
+from dotenv import load_dotenv
+
+load_dotenv()
+
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+db_host = os.getenv("DB_HOST")
+db_name = os.getenv("DB_NAME")
+
+obs_bucket = os.getenv("OBS_BUCKET")
+backup_path_dir = os.getenv("BACKUP_PATH")
+
+backup_filename = f"{db_name}_backup_{datetime.now().strftime('%Y%m%d_%H%M%S')}.sql"
+backup_path = os.path.join(backup_path_dir, backup_filename)
+obs_object_key = f"backups/{backup_filename}"
+
+def dump_database():
+    """Create File dump from database"""
+    dump_command = [
+        "mysqldump",
+        "-u", db_user,
+        f"-p{db_password}",
+        "-h", db_host,
+        db_name,
+        "--single-transaction",
+        "--quick",
+        "--routines",
+        "--triggers",
+        "--events",
+        "--add-drop-database",
+        "--add-drop-table",
+        "--hex-blob",
+        "--result-file", backup_path
+    ]
+    
+    try:
+        print("Creating file dump...")
+        subprocess.run(dump_command, check=True)
+        print(f"Create file dump success: {backup_path}")
+    except subprocess.CalledProcessError as e:
+        print("Dump File Fail : ", e)
+        return False
+    return True
+
+def upload_to_obs():
+    """Upload file to OBS by obsutil"""
+    upload_command = [
+        "obsutil", 
+        "cp", 
+        backup_path, 
+        f"obs://{obs_bucket}/{obs_object_key}"
+    ]
+    
+    try:
+        print("Uploading file to OBS...")
+        subprocess.run(upload_command, check=True)
+        print(f"Success upload file to OBS: {obs_object_key}")
+    except subprocess.CalledProcessError as e:
+        print("Uploadfile to OBS Fail:", e)
+        return False
+    return True
+
+def remove_backup_file():
+    """Remove file backup after upload"""
+    try:
+        os.remove(backup_path)
+        print(f"Remove backup Success: {backup_path}")
+    except OSError as e:
+        print("Fail remove backup file:", e)
+
+if __name__ == "__main__":
+    if dump_database():
+        if upload_to_obs():
+            remove_backup_file()
